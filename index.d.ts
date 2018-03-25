106add5e35ce8f878f424e361bb3a5b620875c5c
Gaubee <gaubeebangeel@gmail.com>
2018-03-25 15:18:20 +0800

    修复了ts.d的一些错误
    Signed-off-by: Gaubee <gaubeebangeel@gmail.com>

--- index.d.ts
+++ index.d.ts
@@ -7,7 +7,6 @@ interface InspectOptions {
 	maxArrayLength?: number | null;
 	breakLength?: number;
 }
-type group_flag = any;
 type ConsoleOptions = {
 	async_log?: boolean;
 	auto_reduce_indent?: boolean;
@@ -18,14 +17,16 @@ export declare class ConsolePro {
 	constructor(opts?: ConsoleOptions);
 	assert(value: any, message?: string, ...optionalParams: any[]): void;
 	debug(message?: any, ...optionalParams: any[]): void;
+	success(message?: any, ...optionalParams: any[]): void;
+	warn(message?: any, ...optionalParams: any[]): void;
 	error(message?: any, ...optionalParams: any[]): void;
 	info(message?: any, ...optionalParams: any[]): void;
 	dir(message?: any, options?: InspectOptions): void;
 	log(message?: any, ...optionalParams: any[]): void;
-	time(label: string): void;
-	timeEnd(label: string): void;
-	group(label: string): group_flag;
-	groupEnd(group_flag: group_flag, label: string): void;
+	time(label: string, ...optionalParams: any[]): Symbol;
+	timeEnd(group_flag: Symbol, label?: string): void;
+	group(label: string, ...optionalParams: any[]): Symbol;
+	groupEnd(group_flag: Symbol, label?: string): void;
 	trace(message?: any, ...optionalParams: any[]): void;
 	warn(message?: any, ...optionalParams: any[]): void;
 	flag(flag: string, message?: any, ...optionalParams: any[]): void;
@@ -36,8 +37,17 @@ export declare class ConsolePro {
 			waiting_msg?: string;
 			useArrowKeys_msg?: string;
 		}
-	);
+	): TerminalMenu;
+	getLine(
+		question: string,
+		filter?: (answer: string) => boolean
+	): Promise<string>;
 	line(message?: any, ...optionalParams: any[]): void;
 	clear(): void;
 	silence(to_be_silence: boolean): void;
+}
+
+declare class TerminalMenu {
+	constructor(title: string, opts: any, logger: any);
+	addOption(name: string, value: string): void;
 }
